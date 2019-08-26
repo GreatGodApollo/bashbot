c4e21fd95a656cd685aa1cfbeedd259de0a019ba
Brett Bender <brett@brettbender.me>
2019-08-26 00:02:18 -0500

    QOL config comments

--- _config.py
+++ _config.py
@@ -1,10 +1,10 @@
 class Config:
-    token = "Your token here"
-    description = "A cool bot that acts like bash"
-    owners = ["YourID"]
-    prefixes = ["./"]
+    token = "Your token here"  # Retrieved from Discord Developer's portal
+    description = "A cool bot that acts like bash"  # Generic description of what your bot does
+    owners = ["YourID"]  # Your Discord user ID
+    prefixes = ["./"]  # Bot prefix
     cogs = ["cogs.owner", "cogs.apt", "cogs.misc", "cogs.administration", "cogs.moderation", "cogs.config",
-            "cogs.hidden", "cogs.repl"]
-    dburl = "DB Connection URL"
-    workingDirectory = "/your/working/directory/"
-    serviceName = "yourServiceName"
+            "cogs.hidden", "cogs.repl"]  # List of cogs bot should initially load
+    dburl = "DB Connection URL"  # Crafted using the sqlalchemy docs
+    workingDirectory = "/your/working/directory/"  # Trailing slash is important!
+    serviceName = "yourServiceName"  # Systemctl service name
