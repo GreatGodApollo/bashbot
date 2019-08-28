a6a8dcd19f643d592b0880438428b87a4023b12f
Brett Bender <brett@brettbender.me>
2019-08-27 23:52:24 -0500

    Default to 6 on roll

--- cogs/random.py
+++ cogs/random.py
@@ -10,16 +10,16 @@ class Random:
         self.bot = bot
 
     @commands.command(pass_context=True)
-    async def roll(self, ctx, sides: int):
+    async def roll(self, ctx, sides: int = 6):
         """Roll a die"""
         await self.bot.say(f"You rolled a {sides} sided die.\n> {random.randint(1, sides)}")
-
 
     @commands.command(pass_context=True)
     async def choose(self, ctx, *choices):
         """Get a random choice"""
         choice = choices[random.randint(0, len(choices) - 1)]
         await self.bot.say(f"I choose\n> {choice}")
+
 
 def setup(bot):
     bot.add_cog(Random(bot))
